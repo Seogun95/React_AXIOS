bc59998c7e8402353ec91abc3095fc0d85085cc2
Seogun95 <ksj083895@gmail.com>
2023-02-18 03:47:47 +0900

    feat: AXIOS POST 구현

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import axios from 'axios'; // axios import
 
 const App = () => {
   const [todos, setTodos] = useState(null);
+  const [inputTitle, setInputTitle] = useState({
+    title: '',
+  });
 
   // 1. axios를 통해서 get 요청을 하는 함수를 생성
   // 비동기처리를 해야하므로 async/await 구문을 통해서 처리합
@@ -22,14 +25,35 @@ const App = () => {
   // 3. data fetching이 정상적으로 되었는지 콘솔을 통해 확인
   console.log(todos);
 
+  //4. onChangesHandler
+  const onChangesHandler = (e) => {
+    setInputTitle({ title: e.target.value });
+  };
+
+  //5. onSubmitHandler
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    //버튼 클릭시 INPUT값을 DB에 추가(POST)
+    axios.post('http://localhost:4001/todos', inputTitle);
+    //setTodos를 업데이트 한다.
+    setTodos([...todos, inputTitle]);
+  };
   return (
-    <div>
-      {todos?.map((item, i) => (
-        <div key={i}>
-          <h1>{`${item.id}: ${item.title}`}</h1>
-        </div>
-      ))}
-    </div>
+    <>
+      <div>
+        <form onSubmit={onSubmitHandler}>
+          <input value={inputTitle.title} onChange={onChangesHandler} />
+          <button>추가</button>
+        </form>
+      </div>
+      <div>
+        {todos?.map((item) => (
+          <div>
+            <p key={item.id}>{`${item.id}: ${item.title}`}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
