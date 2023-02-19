fff333312d35b0feda3450f00174e3ec166403fb
Seogun95 <ksj083895@gmail.com>
2023-02-19 14:21:56 +0900

    feat: axios interceptor 구현

--- src/axios/api.js
+++ src/axios/api.js
@@ -4,6 +4,33 @@ import axios from 'axios';
 // configuration
 const instance = axios.create({
   baseURL: 'http://localhost:4001',
+  // timeout: 1,
 });
+
+instance.interceptors.request.use(
+  function (config) {
+    // 요청을 보내기 전 수행
+    console.log('인터셉트 요청 성공!');
+    return config;
+  },
+  function (error) {
+    // 오류 요청을 보내기 전 수행
+    console.log('인터셉트 요청 오류!');
+    return Promise.reject(error);
+  }
+);
+
+instance.interceptors.response.use(
+  function (response) {
+    console.log('인터넵트 응답 성공!');
+    // 정상 응답
+    return response;
+  },
+
+  function (error) {
+    console.log('인터셉트 응답 오류!');
+    return Promise.reject(error);
+  }
+);
 
 export default instance;
